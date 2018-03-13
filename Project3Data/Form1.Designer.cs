a33377a0705a2e32dae84ce1301467b50cc3b2ee
jjori8 <0946664@hr.nl>
2018-03-13 11:12:48 +0100

    leslie gay

--- Project3Data/Form1.Designer.cs
+++ Project3Data/Form1.Designer.cs
@@ -14,6 +14,7 @@
         /// gavin push test
         /// test 777777
         /// test new
+        /// //hoi leslie gay
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
