1f68bfda3b5308c9b934f4b3fac45bf80508db0d
jjori8 <0946664@hr.nl>
2018-03-13 11:03:40 +0100

    test push

--- Project3Data/Form1.Designer.cs
+++ Project3Data/Form1.Designer.cs
@@ -12,6 +12,7 @@
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         /// gavin push test
+        /// test 777777
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
