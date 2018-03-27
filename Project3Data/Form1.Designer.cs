0bc7f76ceba8f3991b6b532ecfca7b074a08f6b9
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2018-03-27 11:10:17 +0200

    Removed weird piechart

--- Project3Data/Form1.Designer.cs
+++ Project3Data/Form1.Designer.cs
@@ -35,19 +35,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
             this.firefoxSubTabControl1 = new FirefoxSubTabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.firefoxSubTabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 563);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add Parkingmodels to database";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // firefoxSubTabControl1
             // 
@@ -56,9 +60,10 @@
             this.firefoxSubTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.firefoxSubTabControl1.ItemSize = new System.Drawing.Size(100, 40);
             this.firefoxSubTabControl1.Location = new System.Drawing.Point(0, 0);
+            this.firefoxSubTabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.firefoxSubTabControl1.Name = "firefoxSubTabControl1";
             this.firefoxSubTabControl1.SelectedIndex = 0;
-            this.firefoxSubTabControl1.Size = new System.Drawing.Size(1893, 1052);
+            this.firefoxSubTabControl1.Size = new System.Drawing.Size(1262, 664);
             this.firefoxSubTabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.firefoxSubTabControl1.TabIndex = 1;
             // 
@@ -67,43 +72,15 @@
             this.tabPage1.BackColor = System.Drawing.Color.White;
             this.tabPage1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tabPage1.BackgroundImage")));
             this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.tabPage1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.tabPage1.Location = new System.Drawing.Point(8, 48);
+            this.tabPage1.Location = new System.Drawing.Point(4, 44);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1877, 996);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(1254, 616);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Parkeer & Weer";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(7, 880);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(387, 58);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Add Parkingmodels to database";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(282, 151);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(300, 300);
-            this.chart1.TabIndex = 1;
-            this.chart1.Text = "chart1";
             // 
             // tabPage2
             // 
@@ -112,34 +89,30 @@
             this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.tabPage2.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.tabPage2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(79)))), ((int)(((byte)(90)))));
-            this.tabPage2.Location = new System.Drawing.Point(8, 48);
+            this.tabPage2.Location = new System.Drawing.Point(4, 44);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1877, 996);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(1254, 616);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(1262, 664);
             this.Controls.Add(this.button1);
-            this.ClientSize = new System.Drawing.Size(1893, 1052);
             this.Controls.Add(this.firefoxSubTabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Project 3, Team 1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.firefoxSubTabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -150,7 +123,6 @@
         private FirefoxSubTabControl firefoxSubTabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
